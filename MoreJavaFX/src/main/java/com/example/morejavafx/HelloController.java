a3a99c3e9d6a09b6ad1f6c2bd2f4af6a2c689979
Eric Charnesky <eric.charnesky@careevolution.com>
2022-03-15 11:01:05 -0400

    more javafx

---
+++ MoreJavaFX/src/main/java/com/example/morejavafx/HelloController.java
@@ -0,0 +1,107 @@
+package com.example.morejavafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+
+    private Coffee coffee;
+
+    @FXML
+    private Label coffeeLabel;
+
+    @FXML
+    private Button downButton;
+
+    @FXML
+    private Button leftButton;
+
+    @FXML
+    private Button rightButton;
+
+    @FXML
+    private Button upButton;
+
+    // essentially like a constructor, but it runs AFTER all the javafx stuff has been created
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        coffee = new Coffee(250, 50);
+        enableAllButtons();
+    }
+
+    @FXML
+    void downButtonClicked(ActionEvent event) {
+        coffee.setCurrentCoffeeInMilliliters(coffee.getCurrentCoffeeInMilliliters() - 1);
+        enableAllButtons();
+
+        downButton.setDisable(true);
+    }
+
+    @FXML
+    void leftButtonClicked(ActionEvent event) {
+        enableAllButtons();
+        leftButton.setDisable(true);
+    }
+
+    @FXML
+    void rightButtonClicked(ActionEvent event) {
+        enableAllButtons();
+        rightButton.setDisable(true);
+    }
+
+    @FXML
+    void upButtonClicked(ActionEvent event) {
+        coffee.setCurrentCoffeeInMilliliters(coffee.getCurrentCoffeeInMilliliters() + 1);
+
+        enableAllButtons();
+
+        upButton.setDisable(true);
+    }
+
+
+    @FXML
+    void loadButtonClicked(ActionEvent event) {
+        try {
+            ObjectInputStream binaryInputFile = new ObjectInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream("save.bin")));
+            coffee = (Coffee) binaryInputFile.readObject();
+            enableAllButtons();
+            binaryInputFile.close();
+
+        } catch (IOException ex) {
+            System.out.println(ex);
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @FXML
+    void saveButtonClicked(ActionEvent event) {
+        try {
+            // object output stream for writing serializable classes
+            ObjectOutputStream binaryOutputFile = new ObjectOutputStream(
+                    new BufferedOutputStream(
+                            new FileOutputStream("save.bin")));
+            binaryOutputFile.writeObject(coffee);
+            binaryOutputFile.close();
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    private void enableAllButtons() {
+        upButton.setDisable(false);
+        downButton.setDisable(false);
+        leftButton.setDisable(false);
+        rightButton.setDisable(false);
+        coffeeLabel.setText("Coffee: " + coffee.getCurrentCoffeeInMilliliters());
+    }
+}
